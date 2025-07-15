9f53a5a9bb71da38a594240357e29f2eb5d9b196
Jerome Avecilla <102340560+javecilla@users.noreply.github.com>
2025-07-15 05:02:01 -0700

    Update info.blade.php

--- info.blade.php
+++ info.blade.php
@@ -1,5 +1,5 @@
-<x-layout.guest title="About">
-    <x-slot name="description">
-        For confidentiality of this project src code is not showing in this repository.
-    </x-slot>
+<x-layout.guest title="Info">
+    <p><strong>Notice:</strong> This repository is limited to documentation and licensing files only.</p>
+    <p>The source code is kept private for confidentiality and academic integrity purposes.</p>
+    <p>For more information, please refer to the <a href="./README.md" target="_blank">README</a> or contact the project contributors.</p>
 </x-layout.guest>
